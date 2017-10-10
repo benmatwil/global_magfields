400b09e2657f712a1cd402973e1075bf596f6b0f
Benjamin Williams <bmw@st-andrews.ac.uk>
2017-10-10 18:11:49 +0100

    Added ability to deal with carrington rotation maps

--- synopticmap.py
+++ synopticmap.py
@@ -3,8 +3,10 @@ import numpy as np
 from astropy.io import fits
 import re, glob
 import datetime as dt
+import matplotlib.pyplot as plt
+import os.path
 
-def get_pole_value(date, pole=None, average=15):
+def get_pole_value(date, pole=None, average=15, maptype=None, rot_num=None):
     if pole == 'n':
         headerarg = 'BANDN3'
     elif pole == 's':
@@ -12,11 +14,28 @@ def get_pole_value(date, pole=None, average=15):
     else:
         print("Haven't given either north ('n') or south ('s') to function")
 
-    datelist = [dt.datetime(int(date[:4]), int(date[4:6]), int(date[6:8])) + dt.timedelta(days=x) for x in range(-15, 15)]
+    if maptype == 'daily':
+        startnum = -average
+        endnum = average
+    elif maptype == 'cr':
+        index = np.where(carr_rots['number'] == rot_num+1)[0][0]
+        carr_rot = carr_rots[index]
+        date2 = str(carr_rot['year']) + '{:02d}'.format(carr_rot['month']) + '{:02d}'.format(int(carr_rot['day']))
+        date2 = dt.datetime(int(date2[:4]), int(date2[4:6]), int(date2[6:8]))
+        startnum = 0
+        endnum = (date2 - dt.datetime(int(date[:4]), int(date[4:6]), int(date[6:8]))).days
 
-    value = np.mean(np.array([fits.open('hmi/fits/mean/hmi.meanpf_720s.{}_000000_TAI.mf_br.fits'.format(day.strftime('%Y%m%d')))[0].header[headerarg] for day in datelist]))
+    datelist = [dt.datetime(int(date[:4]), int(date[4:6]), int(date[6:8])) + dt.timedelta(days=x) for x in range(startnum, endnum)]
+
+    values = []
+    for day in datelist:
+        fname = 'hmi/fits/mean/hmi.meanpf_720s.{}_000000_TAI.mf_br.fits'.format(day.strftime('%Y%m%d'))
+        if os.path.isfile(fname):
+            values.append(fits.open(fname)[0].header[headerarg])
     
-    return value
+    mean = np.mean(np.array(values))
+    
+    return mean
 
 def pole_filler(map, start, fillvalue):
     """
@@ -72,52 +91,87 @@ def smooth_map(map):
     return map
 
 # Read in Synoptic map and fill/div(B) = 0 appropriately
-dir = 'hmi'
+def run(filesearch, dir='hmi', maptype=None):
+    global carr_rots
 
-for fitsfilename in glob.glob(dir+'/fits/hmi.Mrdailysynframe_720s.*'):
+    if maptype is None:
+        print("Please give a Synoptic map type e.g. maptype='daily'")
+        return
 
-    if fitsfilename.find('daily') > -1:
-        # type = 'daily'
-        date = re.findall(r"\d{8}", fitsfilename)[0]
-    else:
-        # type = 'crot'
-        date = re.findall(r"\d{4}", fitsfilename)[0]
+    if maptype == 'cr':
+        carr_rots = np.loadtxt('hmi/fits/mean/carr_rot_dates.txt', comments='#', dtype=[('number', np.int32), ('year', np.int32), ('month', np.int32), ('day', np.float64), ('date', np.float64), ('jul_date', np.float64)])
 
-    fitsfile = fits.open(fitsfilename)
-    br1 = fitsfile[0].data.transpose()
-    fitsfile.close(fitsfilename)
-    br1 = br1.astype(np.float64)
-    nlon = br1.shape[0]
-    nlat = br1.shape[1]
-    lons = np.linspace(0,2*np.pi,nlon+1)
-    lats = np.linspace(-1,1,nlat+1)
-    lons = (lons[1:] + lons[:-1])/2
-    lats = (lats[1:] + lats[:-1])/2
-    print('Synoptic map {} of dimensions {} by {}'.format(fitsfilename, nlon, nlat))
+    for fitsfilename in glob.glob(dir + '/fits/' + filesearch):
 
-    # fill nans
-    # The south pole is the 0 index and north pole nlon/-1 index
-    # br_north = -30 * 1e-6 * 1e4 # -30 microT to G
-    # br_south = 50 * 1e-6 * 1e4 # 50 microT to G
-    br_north = get_pole_value(date, pole='n')
-    br_south = get_pole_value(date, pole='s')
+        # if fitsfilename.find('daily') > -1:
+        if maptype == 'daily':
+            date = re.findall(r"\d{8}", fitsfilename)[0]
+        elif maptype == 'cr':
+            date = re.findall(r"\d{4}", fitsfilename)[0]
 
-    print(br_north, br_south)
+        fitsfile = fits.open(fitsfilename)
+        br1 = fitsfile[0].data.transpose()
+        fitsfile.close(fitsfilename)
+        br1 = br1.astype(np.float64)
+        nlon = br1.shape[0]
+        nlat = br1.shape[1]
+        lons = np.linspace(0,2*np.pi,nlon+1)
+        lats = np.linspace(-1,1,nlat+1)
+        lons = (lons[1:] + lons[:-1])/2
+        lats = (lats[1:] + lats[:-1])/2
+        print('Synoptic map {} of dimensions {} by {}'.format(fitsfilename, nlon, nlat))
 
-    br1 = pole_filler(br1, 0, br_south)
-    br1 = pole_filler(br1, -1, br_north)
-    
-    #br1 = smooth_map(br1)
+        # fill nans
+        # The south pole is the 0 index and north pole nlon/-1 index
+        # br_north = -30 * 1e-6 * 1e4 # -30 microT to G
+        # br_south = 50 * 1e-6 * 1e4 # 50 microT to G
+        datestr = date
+        rot_num = None
+        if maptype == 'cr':
+            index = np.where(carr_rots['number'] == int(date))[0][0]
+            carr_rot = carr_rots[index]
+            datestr = str(carr_rot['year']) + '{:02d}'.format(carr_rot['month']) + '{:02d}'.format(int(carr_rot['day']))
+            rot_num = carr_rot[0]
+        
+        br_north = get_pole_value(datestr, pole='n', maptype=maptype, rot_num=rot_num)
+        br_south = get_pole_value(datestr, pole='s', maptype=maptype, rot_num=rot_num)
 
-    # make div(B) = 0
-    flat = br1.flatten()
-    br1 -= np.sum(flat[np.argsort(abs(flat))])/nlon/nlat
+        print(br_north, br_south)
 
-    # write to file in fortran order
-    with open(dir+'/synmap_' + date + '.dat', 'wb') as savefile:
-        np.array([nlon],dtype=np.int32).tofile(savefile)
-        np.array([nlat],dtype=np.int32).tofile(savefile)
-        for i in range(nlat):
-            br1[:,i].tofile(savefile) # fortran order
-        lons.tofile(savefile)
-        lats.tofile(savefile)
+        br1 = pole_filler(br1, 0, br_south)
+        br1 = pole_filler(br1, -1, br_north)
+        
+        #br1 = smooth_map(br1)
+        # if True:
+        #     flat = br1.flatten()
+        #     #for i in range(10):
+        #     #    ibin = [10**(i-8),10**(i-7)]
+        #     #    plt.hist(np.abs(flat), bins = ibin)
+        #     #plt.gca().set_xscale("log")
+        #     #plt.gca().set_yscale("log")
+        #     for i in range(10000):
+        #         ibin = [i*10**(-3), (i+1)*10**(-3)]
+        #         plt.hist(np.abs(flat), bins = ibin)
+        #     plt.axvline(np.sum(flat[np.argsort(abs(flat))])/nlon/nlat, color='k')
+        #     plt.xlabel('Absolute Value of Pixels')
+        #     plt.ylabel('Number of Pixels')
+        #     plt.gcf().set_size_inches([12/2.55,9/2.55])
+        #     plt.tight_layout()
+        #     plt.savefig('pixeldistnew.pgf')
+
+        #     print(np.sum(flat[np.argsort(abs(flat))])/nlon/nlat)
+        #     print(len(np.where(np.abs(flat) < 1e-7)))
+        #     a = b
+
+        # make div(B) = 0
+        flat = br1.flatten()
+        br1 -= np.sum(flat[np.argsort(abs(flat))])/nlon/nlat
+
+        # write to file in fortran order
+        with open(dir+'/synmap_' + date + '.dat', 'wb') as savefile:
+            np.array([nlon],dtype=np.int32).tofile(savefile)
+            np.array([nlat],dtype=np.int32).tofile(savefile)
+            for i in range(nlat):
+                br1[:,i].tofile(savefile) # fortran order
+            lons.tofile(savefile)
+            lats.tofile(savefile)
